3bedcffcf9a363e0da6b152933d54ad4be6a3760
Mohammad Mojahed <99251464+mohmojahed@users.noreply.github.com>
2022-08-05 20:40:53 +0200

    Commit from Mohammad

--- src/Group17.java
+++ src/Group17.java
@@ -4,6 +4,10 @@ public class Group17 {
 
         //Haden Z.
 
+        System.err.println("Mohammad Not Found :D");
+
+
         
     }
+
 }
---
+++ src/Mohammad/Day_FirstProject.java
@@ -0,0 +1,10 @@
+package Mohammad;
+
+public class Day_FirstProject {
+
+    public static void main(String[] args) {
+
+        System.err.println("Error 404! Mohammad not found");
+
+    }
+}
