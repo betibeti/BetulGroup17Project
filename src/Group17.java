c831043afeb1eb6e7f71b82392e698372bc7fa45
ahalu <a.haluk@hotmail.de>
2022-08-05 20:40:22 +0200

    Created new Class

--- src/Group17.java
+++ src/Group17.java
@@ -4,6 +4,5 @@ public class Group17 {
 
         //Haden Z.
 
-        
     }
 }
---
+++ src/NewClass123.java
@@ -0,0 +1,8 @@
+public class NewClass123 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hi Guys and Girls");
+
+    }
+}
